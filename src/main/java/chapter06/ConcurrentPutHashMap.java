91d1f713c675cf1b26c857cc2b7584504e2064ac
moqi <moqimoqidea@gmail.com>
2021-08-28 16:33:46 +0800

    Update

--- src/main/java/chapter06/ConcurrentPutHashMap.java
+++ src/main/java/chapter06/ConcurrentPutHashMap.java
@@ -4,34 +4,31 @@
  */
 package chapter06;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.UUID;
 
 /**
  * 并发put
- * 
+ *
  * @author tengfei.fangtf
  * @version $Id: Snippet.java, v 0.1 2015-7-31 下午11:53:55 tengfei.fangtf Exp $
  */
+@Slf4j
 public class ConcurrentPutHashMap {
 
     public static void main(String[] args) throws InterruptedException {
-        final HashMap<String, String> map = new HashMap<String, String>(2);
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10000; i++) {
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            map.put(UUID.randomUUID().toString(), "");
-                        }
-                    }, "ftf" + i).start();
-                }
+        final HashMap<String, String> map = new HashMap<>(2);
+        Thread t = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                new Thread(() -> map.put(UUID.randomUUID().toString(), ""), "ftf" + i).start();
             }
         }, "ftf");
         t.start();
         t.join();
+
+        log.info("map.size: {}", map.size());
     }
 
 }
